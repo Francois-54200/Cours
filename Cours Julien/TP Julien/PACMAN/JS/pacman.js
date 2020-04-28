705d7387466a32ff49a386fed861991f712ebf3f
Steinbach Francois <steinbach.francois@neuf.fr>
2020-04-28 14:21:44 +0200

    init projet

--- Cours Julien/TP Julien/PACMAN/JS/pacman.js
+++ Cours Julien/TP Julien/PACMAN/JS/pacman.js
@@ -8,9 +8,9 @@ let maGrille = [
     [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
     [0,0,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,0,0],
     [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
-    [0,0,0,0,2,0,2,0,0,2,0,0,2,0,2,0,0,0,0],
-    [2,2,2,2,2,2,2,0,2,2,2,0,2,2,2,2,2,2,2],
-    [0,0,0,0,2,0,2,0,0,2,0,0,2,0,2,0,0,0,0],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [2,2,2,2,2,2,2,0,1,1,1,0,2,2,2,2,2,2,2],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
     [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
     [0,0,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,0,0],
     [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
@@ -27,10 +27,12 @@ let maGrille = [
   let pacman = {
     //position de départ de Pacman sur la grille
     y:2,
-    x:5,
+    x:1,
     //direction de départ de pacman (haut:4, bas:2, gauche:3, droite:1)
     direction:1
 }
+
+    let score = 0
 
 function afficheGrille(){
     document.getElementById('grille').innerHTML = " ";
@@ -71,23 +73,76 @@ function bougePacman(){
     if (pacman.direction == 1){
         pacman.x++
     }
+
+    else if (pacman.direction ==2) {
+        pacman.y++
+    }
+
+    else if (pacman.direction ==3) {
+        pacman.x--
+    }
+
+    else if (pacman.direction ==4) {
+        pacman.y--
+    }
         
     if(maGrille[pacman.y-1][pacman.x-1]==0)
 {
-        // là pacman est dans un mur
+    if (pacman.direction == 1){
+        pacman.x--
+    }
+
+    else if (pacman.direction ==2) {
+        pacman.y--
+    }
+
+    else if (pacman.direction ==3) {
+        pacman.x++
+    }
+
+    else if (pacman.direction ==4) {
+        pacman.y++
+    }  // là pacman est dans un mur
 }
+    if(maGrille[pacman.y-1][pacman.x-1]==2){
+        maGrille[pacman.y-1][pacman.x-1]=1
+        score=score+1  //ou score++
+    }
+}
+
+function appuiTouche(e){
+    console.log(e.key)
+    if(e.key=="z"){
+        pacman.direction=4
+    }
+    else if(e.key=="q"){
+        pacman.direction=3
+    }
+    else if(e.key=="s"){
+        pacman.direction=2
+    }
+    else if(e.key=="d"){
+        pacman.direction=1
+    }
+}
+
+function afficheScore(){
+    document.getElementById('score').innerHTML=score
+
 }
 
 
 function refresh(){
-    setTimeout(refresh, 150)
-    afficheGrille()
-    affichePacman()
+    setTimeout(refresh, 550)
     bougePacman()
+    afficheGrille()
+    affichePacman()    
+    afficheScore()
 
-    console.log("je rafraichi ma page")
+   // console.log("je rafraichi ma page")
 }
 refresh()
+document.body.addEventListener("keydown",appuiTouche)
 
 
 
