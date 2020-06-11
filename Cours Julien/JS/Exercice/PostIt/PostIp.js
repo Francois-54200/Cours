1dc28bb4f35ce6fd3194e21eb0152777f5418eaa
Steinbach Francois <steinbach.francois@neuf.fr>
2020-06-11 14:08:53 +0200

    initprojet

---
+++ Cours Julien/JS/Exercice/PostIt/PostIp.js
@@ -0,0 +1,12 @@
+class Post_it {
+    largeur;
+    hauteur;
+    couleurtexte;
+    Couleurdefond;
+    x;
+    y; 
+    constructor(largeur, hauteur, Couleurdefond) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.Couleurdefond = Couleurdefond;
+    }
